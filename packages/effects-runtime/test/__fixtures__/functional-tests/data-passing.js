bb4115a97c7c91c35db2a5c517494c649de9e88c
Dave Campion <me@davecampion.com>
2020-03-03 18:54:07 -0800

    fix/effect-handler-regression
    * Fixed regression introduced by the grammar swap
    * Updated examples

---
+++ packages/effects-runtime/test/__fixtures__/functional-tests/data-passing.js
@@ -0,0 +1,44 @@
+const main = () => {
+    'use effects';
+    try{
+        return perform {type : 'effect', data : 'data'}
+    }handle 'effect' with (e){
+        recall e.data
+    }
+};
+
+const dynamicEffect = (effect) => {
+    'use effects';
+    try{
+        return perform effect;
+    }handle 'effectA' with (e){
+        recall 'effectA';
+    }handle 'effectB' with (e){
+        if(e.data === 1){
+            recall true;
+        } else {
+            recall false;
+        }
+    }
+}
+module.exports.test = ({it, expect}) => {
+    it('Should pass data into handler variable', async () => {
+        const result = await main();
+
+        expect(result).toBe('data');
+    });
+
+    it('Should respond to dynamic effect passing', async () => {
+        const result = await dynamicEffect({type : 'effectA'});
+
+        expect(result).toBe('effectA');
+    });
+
+    it('Should recall dynamically', async () => {
+        const resultA = await dynamicEffect({type : 'effectB', data : 1});
+        const resultB = await dynamicEffect({type : 'effectB', data : 2});
+
+        expect(resultA).toBe(true);
+        expect(resultB).toBe(false);
+    })
+};
