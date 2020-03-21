604515d4c8d9a27dc0e8e96d48649df3b4f38936
cdaringe <cdaringe@gmail.com>
2020-03-21 01:30:19 -0700

    test(runtime): test concurrent stacks

---
+++ packages/effects-runtime/test/__fixtures__/functional-tests/concurrent-async-identity.js
@@ -0,0 +1,29 @@
+const delay = ms => new Promise(res => setTimeout(res, ms))
+const asyncIdentity = name => {
+  return perform { type: 'async_identity', payload: { name } }
+}
+
+const work = async (name) => {
+  'use effects'
+  try {
+    return asyncIdentity(name)
+  } handle 'async_identity' with (e) {
+    const { payload: { name } } = e
+    await delay(name === 'a' ? 100 : 1)
+    recall name
+  }
+}
+
+function main () {
+  const aP = work('a')
+  const bP = work('b')
+  const cP = work('c')
+  return Promise.all([aP, bP, cP])
+}
+
+module.exports.test = ({it, expect}) => {
+  it('Should return out-of-order, concurrent effect-roots back in called order', async () => {
+    const result = await main();
+    expect(result).equals(['a', 'b', 'c']);
+  });
+};
